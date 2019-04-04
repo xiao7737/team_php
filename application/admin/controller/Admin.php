ef212a3965a3b0d45b95921687fe4580dc82fc0a
xiao <773708486@qq.com>
2019-04-04 23:03:35 +0800

    解析参数

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -16,6 +16,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Request;
 
 /**
  * todo 球队管理员操作：注册，登录，球队管理，球员管理
@@ -24,17 +25,18 @@ use think\Controller;
  */
 class Admin extends Controller
 {
-    public function register()  //todo phone+pw
+    public function register()  //todo phone+pw        如果已经继承基类，则已经自动完成了构造方法注入
     {
         return $this->request->param('name1');  //获取参数
-       /* return json(['admin_id' => 1,
-                     'status '  => 1,
-                     'username' => '肖西川'
-        ]);*/                                   //返回结果格式
+        //return request()->param('name');      //也可以直接调用request()函数
+        /* return json(['admin_id' => 1,
+                      'status '  => 1,
+                      'username' => '肖西川'
+         ]);*/
     }
 
-    public function login()
+    public function login(Request $request)        //也可以使用在方法中使用依赖注入
     {
-        return json("login test");
+        return json($request->param('name'));
     }
 }
