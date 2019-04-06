42cd89631153318a447605b8a1cc23eba8f540da
xiao <773708486@qq.com>
2019-04-06 09:47:33 +0800

    request test

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -16,7 +16,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
-use think\Request;
+use think\facade\Request;
 
 /**
  * todo 球队管理员操作：注册，登录，球队管理，球员管理
@@ -27,8 +27,10 @@ class Admin extends Controller
 {
     public function register()  //todo phone+pw        如果已经继承基类，则已经自动完成了构造方法注入
     {
-        return $this->request->param('name1');  //获取参数
+        //return $this->request->param('name1');  //获取参数
         //return request()->param('name');      //也可以直接调用request()函数
+        //return Request::param('name');          //也可以使用静态调用的方式，但是注意use的是facade
+        return Request::url(true);
         /* return json(['admin_id' => 1,
                       'status '  => 1,
                       'username' => '肖西川'
