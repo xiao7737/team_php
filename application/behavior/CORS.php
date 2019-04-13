a42fd6a80d14cadf77339af0a415328fd3b0dc46
xiao <773708486@qq.com>
2019-04-13 10:38:40 +0800

    解决访问跨域

---
+++ application/behavior/CORS.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * User: xiaoxc
+ * Date: 2019/4/13
+ * Time: 10:36
+ * 　　   ┏┓　 ┏┓
+ * 　┏━━━━┛┻━━━┛┻━━━┓
+ * 　┃              ┃
+ * 　┃       ━　    ┃
+ * 　┃　  ┳┛ 　┗┳   ┃
+ * 　┃              ┃
+ * 　┃       ┻　    ┃
+ * 　┗━━━┓      ┏━━━┛
+ */
+
+namespace app\behavior;
+
+use think\Response;
+
+/**
+ * Class CORS
+ * @package app\behavior
+ */
+class CORS
+{
+
+    public function appInit()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: token, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        header('Access-Control-Allow-Methods: POST,GET,PUT,DELETE');
+
+        if (request()->isOptions()) {
+            exit();
+        }
+    }
+}
