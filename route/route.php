52b38353d38085ca53032628b226c8d277c50d78
xiao <773708486@qq.com>
2019-05-09 20:41:41 +0800

    debug

--- route/route.php
+++ route/route.php
@@ -19,7 +19,7 @@ Route::group('team', function () {
     Route::post('update_team', 'admin/team/updateTeam');                     //更新球队
     Route::post('get_team_list', 'admin/team/getTeamList');                  //获取球队列表
     Route::post('get_team_by_id', 'admin/team/getTeamById');                 //获取球队信息
-    Route::post('get_member_list', 'admin/team/getMemberList');              //获取球队的球员列表
+    Route::get('get_member_list', 'admin/team/getMemberList');              //获取球队的球员列表
     Route::post('get_team_list_by_fuzzy', 'admin/team/getTeamListByFuzzy');  //根据球队名模糊查询
 })->middleware('app\http\middleware\Auth::class');
 
